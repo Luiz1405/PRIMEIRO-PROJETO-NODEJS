0b2d256fd8911651904de51684beb59fd5ca7f03
Luiz1405 <kubaszewski001@hotmail.com>
2025-03-03 15:16:45 -0300

    Criação e alteração dos endpoints POST e GET

--- Server.js
+++ Server.js
@@ -1,10 +1,21 @@
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = 3001
 
-app.listen(3000)
+const users = []
+
+app.listen(`${port}`)
+app.use(express.json()) 
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
 
 app.get('/usuarios', (req, res) => {
-    res.send('Hello World!')
+
+    res.status(200).json(users)
+
 })
